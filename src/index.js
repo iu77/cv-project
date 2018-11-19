62b06b150d366457d84e7e1ab6e122e42953148b
iu77 <ionelungur0225@gmail.com>
2018-11-19 21:51:17 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,31 @@
+function $(id) {
+  return document.getElementById(id);
+}
+function hide(id) {
+  $(id).style.display = "none";
+}
+function show(id) {
+  $(id).style.display = "block";
+}
+
+function hidePage(el){
+  el.style.display = 'none';
+}
+
+function hideAllPages(){
+  var pages = document.querySelectorAll(".page-block");
+  pages.forEach(hideElement);
+}
+
+var links = document.querySelectorAll("#top-menu-bar a");
+for (var i = 0; i < links.length; i++) {
+  links[i].onclick = function(){
+    hideAllPages();
+    var page = this.getAttribute('data-page');
+    show(page+"-page");
+  };
+}
+
+show('home-page');
+
+
