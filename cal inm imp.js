cfcc3ecca9dd6cbd7aeff4e493e2b336b0a90903
iu77 <ionelungur0225@gmail.com>
2018-11-26 20:15:15 +0200

    inmultit2nr

--- cal inm imp.js
+++ cal inm imp.js
@@ -9,5 +9,12 @@ function divideBy()
 { 
         num1 = document.getElementById("firstNumber").value;
         num2 = document.getElementById("secondNumber").value;
-document.getElementById("result").innerHTML = num1 / num2;
+        document.getElementById("result").innerHTML = num1 / num2;
+}
+
+function sumBy()
+{
+        num1 = document.getElementById("firstNumber").value;
+        num2 = document.getElementById("secondNumber").value;
+        document.getElementById("result").innerHTML = num1+num2;
 }
