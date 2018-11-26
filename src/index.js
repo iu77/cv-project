31b02a6b4f918df7e5e1e8a60ab3110b07a8d3e7
iu77 <ionelungur0225@gmail.com>
2018-11-26 21:49:54 +0200

    cv 26nov

--- src/index.js
+++ src/index.js
@@ -1,37 +1,53 @@
 
-function $(id){
+function $(id) {
   return document.getElementById(id)
 }
 
-function hide(id){
- $(id).style.display = "none";
-  
+function hide(id) {
+  $(id).style.display = "none";
+
 }
 function show(id) {
   $(id).style.display = "block";
 }
 
-
-function hideAllpages(){
+function hideAllpages() {
   var pages = document.querySelectorAll(".page-block");
-  for( var i=0 ; i < pages.length ; i++ ){
-   pages[i].style.display = 'none';
+  for (var i = 0; i < pages.length; i++) {
+    pages[i].style.display = 'none';
   }
 
 }
-var links = document.querySelectorAll("#top-menu-bar a");
-for( var i=0 ; i < links.length ; i++ ){
-  links[i].onclick = function(){
-    console.warn('hide all page');
-    hideAllpages();
-    var page = this.getAttribute('data-page')
-    console.info('show page',page );
-    show(page+"-page");
 
+function initMenu() {
+  var links = document.querySelectorAll("#top-menu-bar a");
+}
+var links = document.querySelectorAll("#top-menu-bar a");
+for (var i = 0; i < links.length; i++) {
+  links[i].onclick = function () {
+    hideAllpages();
+    var page = this.getAttribute('data-page');
+    console.info('show page', page);
+    show(page + "-page");
   };
 
 }
 
+function initSkillsPage() {
+  var skills = ['js', 'html', 'css'];
+  var resultList = document.querySelector('#skills-page ul');
+
+  var listItems=skills.map(function (skill) {
+    return `<li>${skill.toUpperCase()}</li>`;
+
+  })
+
+  resultList.innerHTML = listItems.join('');
+}
+
+initMenu();
+show('skills-page');
+initSkillsPage();
 
 
 
