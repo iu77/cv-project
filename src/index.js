7827f9f8da34d3fd1dfea44f49762dfb03fd8d63
iu77 <ionelungur0225@gmail.com>
2018-12-10 17:07:05 +0200

    skill endroersement

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
+
 function $(id) {
-  return document.getElementById(id)
+  return document.getElementById(id);
 }
 function hide(id) {
   $(id).style.display = "none";
@@ -7,31 +8,52 @@ function hide(id) {
 function show(id) {
   $(id).style.display = "block";
 }
+
+function hideElement(el) {
+  el.style.display = 'none';
+}
+
 function hideAllPages() {
   var pages = document.querySelectorAll(".page-block");
   pages.forEach(hideElement);
 }
+
 function initMenu() {
   var links = document.querySelectorAll("#top-menu-bar a");
-  for (var i = 0; i < links.length; i++) {
-    links[i].onclick = function () {
+  for(var i = 0; i < links.length; i++) {
+    links[i].onclick = function(){
       hideAllPages();
       var page = this.getAttribute('data-page');
       show(page + "-page");
     };
   }
 }
-function initSkillsPage() {
-  var skills = ['electronics', 'engineering', 'html', 'css', 'js'];
-  var resultList= document.querySelector('#skills-page ul');
 
-var listItems=skills.map(function (skill) {
-  return `<li> ${skill.toUpperCase()} </li>`;
-});
-  resultList.innerHTML =listItems.join('');
+function initSkillsPage() {
+  var skills = [
+    ['js',   7, "Andrei"],
+    ['HTML', 6, ""],
+    ['css',  2, ""],
+    ['electronics',  5, "Gheorghe"],
+    ['engeneering',  1  ,  ""]
+
+  ];
+  var resultList = document.querySelector('#skills-page ul');
+
+  var listItems = skills.map(function(skill) {
+    var endorsedBy = ' - Endorsed by';
+    if (skill[2] == "") {
+      endorsedBy = "";
+    }
+    var name = skill[0].toUpperCase();
+    return `<li>${name}
+      <span style="color: gray">- ${skill[1]} ${endorsedBy}</span> 
+      ${skill[2]}
+      </li>`;
+  });
+  resultList.innerHTML = listItems.join('');
 }
 
 initMenu();
 show('skills-page');
 initSkillsPage();
-
